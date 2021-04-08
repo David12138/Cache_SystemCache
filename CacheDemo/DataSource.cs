1154a70549f359b335a48d7e580c88f77592462c
zdw <1732182169@qq.com>
2021-04-08 11:37:24 +0800

    首次提交

---
+++ CacheDemo/DataSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace CacheDemo
+{
+    public class DataSource
+    {
+        /// <summary>
+        /// 模拟从数据库读取数据
+        /// 耗时、耗CPU
+        /// </summary>
+        /// <param name="count"></param>
+        public static int GetDataByDB(int count)
+        {
+            Console.WriteLine("-------开始获取缓存数据-------");
+            int result = 0;
+            for (int i = count; i < 99999999; i++)
+            {
+                result += i;
+            }
+            Thread.Sleep(2000);
+            return result;
+        }
+    }
+}
---
+++ CacheDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CacheDemo
+{
+    /// <summary>
+    /// 控制台方法入口
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 缓存尽量在数据量小、重复查询量大的情况下使用。因为缓存也是要耗内存的，服务器内存是有限的！
+             */
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"------第{i}次请求------");
+                int result = 0;
+
+                string key = "DataSource_GetDataByDB_666";                
+
+                result = CacheHelper.GetCache(key, () => DataSource.GetDataByDB(666));
+                Console.WriteLine($"第{i}次请求获得的数据为：{result}");
+            }
+
+            //让窗体保存接受外部参数的状态来达到不退出的效果
+            Console.ReadKey();
+        }
+    }
+}
